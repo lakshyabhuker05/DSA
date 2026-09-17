87be5ebf1a69a3edf201c3173f31fbd9f9bd0c47
Lakshya <lakshyabhuker@gmail.com>
2026-09-17 15:06:17 +0530

    Update Pattern3.cpp
    Uncommented the code for reading input and printing the pattern.

--- Pattern3.cpp
+++ Pattern3.cpp
@@ -1,22 +1,22 @@
-// #include <iostream>
-// using namespace std;
-// int main(){
+ #include <iostream>
+using namespace std;
+int main(){
 
-//     int n;
-//     cin >> n;
+    int n;
+    cin >> n;
 
-//     for(int i = 0; i < (n-1); i++){
-//         for(int j = 0; j < (n-i-1); j++){
-//             cout << " ";
-//         }
-//         for(int k = 0; k < (2*i+1);k++){
-//             cout << "*";
-//         }
-//         cout << endl;
-//     }
+    for(int i = 0; i < (n-1); i++){
+        for(int j = 0; j < (n-i-1); j++){
+            cout << " ";
+        }
+        for(int k = 0; k < (2*i+1);k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 #include <iostream>
 using namespace std;
