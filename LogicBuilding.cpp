829ae93159bb59513b198964dac5a69d99b9204c
lakshyabhuker05 <lakshyabhuker@gmail.com>
2026-09-22 23:30:35 +0530

    Day 7 - logic building practice

---
+++ LogicBuilding.cpp
@@ -0,0 +1,32 @@
+void pattern8(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < i; j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * (n - i) + 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void pattern9(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
